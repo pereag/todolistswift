fdec655a166c359af70fe462fcf2ef89ab90d774
valc0d3 <perellovalentinpro@gmail.com>
2022-09-30 13:28:46 +0200

    Initial Commit

---
+++ todolist/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  todolist
+//
+//  Created by Valc0d3 on 30/09/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
