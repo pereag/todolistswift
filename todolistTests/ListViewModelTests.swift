a3a127053d4f8b6c23d1ab681be435a71bb82943
valc0d3 <perellovalentinpro@gmail.com>
2023-01-14 15:22:23 +0100

    Update test files

--- todolistTests/ListViewModelTests.swift
+++ todolistTests/ListViewModelTests.swift
@@ -8,70 +8,148 @@
 import XCTest
 @testable import todolist
 
-final class ListViewModelTests: XCTestCase {
-    private var viewModel: ListViewModel!
+import XCTest
+@testable import todolist
+
+class ListViewModelTests: XCTestCase {
     
-    /* func testViewDidLoad() {
-        let expectation = self.expectation(description: "dispay todoList")
-        viewModel = ListViewModel()
-        
-        var counter = 0
-        
-        viewModel.displayTodoList = { todos in
-            if counter == 0 {
-                // verif
-            }
-            
-        }
-        viewModel.viewDidLoad()
-        waitForExpectations(timeout: 1.0)
-        
+    var sut: ListViewModel!
+    var mockRepository: MockListRepository!
+    
+    override func setUp() {
+        super.setUp()
+        mockRepository = MockListRepository()
+        sut = ListViewModel(repository: mockRepository, todos: [])
     }
     
-    func testDidPressAddWhenTodoIsNotEmty() {
-        viewModel = ListViewModel()
-        viewModel.viewDidLoad()
-        viewModel.didPressAdd(todo: "yolo")
-        let a: String = "yolo"
-        
-        XCTAssertEqual(a, "yolo")
-    } */
-    
-    /* func testDidPressAddWhenTodoIsEmty() {
-        
+    override func tearDown() {
+        sut = nil
+        mockRepository = nil
+        super.tearDown()
     }
     
-    func testDidPressRemoveTodoWhenRepositoryIsNotNil() {
-        
+    func testViewDidLoadCallsGetTodos() {
+        sut.viewDidLoad()
+        XCTAssertTrue(mockRepository.getTodosCalled)
     }
     
-    func testDidPressRemoveTodoWhenRepositoryIsNil() {
-        
+    func testViewDidLoadCallsDisplayTodoListWithExpectedTodos() {
+        let expectedTodos = ["Test Todo"]
+        var passedTodos: [String]?
+        mockRepository.stubbedGetTodosResult = expectedTodos
+        sut.displayTodoList = { todos in passedTodos = todos }
+        sut.viewDidLoad()
+        XCTAssertEqual(passedTodos, expectedTodos)
     }
     
-    func testDidPressEditTodo() {
-        
+    func testViewDidLoadCallsDisplayTodoList() {
+        var displayTodoListCalled = false
+        sut.displayTodoList = { _ in displayTodoListCalled = true }
+        sut.viewDidLoad()
+        XCTAssertTrue(displayTodoListCalled)
     }
     
-    func testChangeTodoValueIfIndexIsNil() {
-        
+    func testDidPressAddCallsAddTodo() {
+        sut.didPressAdd(todo: "Test Todo")
+        XCTAssertTrue(mockRepository.addTodoCalled)
     }
     
-    func testChangeTodoValueIfContentIsNotEmpty() {
-        
+    func testDidPressAddCallsGetTodos() {
+        sut.didPressAdd(todo: "Test Todo")
+        XCTAssertTrue(mockRepository.getTodosCalled)
     }
     
-    func testChangeTodoValueIfContentIsEmpty() {
-        
+    func testDidPressAddCallsDisplayTodoList() {
+        var displayTodoListCalled = false
+        sut.displayTodoList = { _ in displayTodoListCalled = true }
+        sut.didPressAdd(todo: "Test Todo")
+        XCTAssertTrue(displayTodoListCalled)
     }
     
-    func testChangeTodoValueIfContentIsEmptyAndRepositoryIsNil() {
-        
+    func testDidPressRemoveTodoCallsRemoveTodo() {
+        sut.didPressRemoveTodo(index: 0)
+        XCTAssertTrue(mockRepository.removeTodoCalled)
     }
     
-    func testChangeTodoValueIfContentIsEmptyAndRepositoryIsNotNil() {
-        
-    } */
+    func testDidPressRemoveTodoCallsGetTodos() {
+        sut.didPressRemoveTodo(index: 0)
+        XCTAssertTrue(mockRepository.getTodosCalled)
+    }
+    
+    func testDidPressRemoveTodoCallsDisplayTodoList() {
+        var displayTodoListCalled = false
+        sut.displayTodoList = { _ in displayTodoListCalled = true }
+        sut.didPressRemoveTodo(index: 0)
+        XCTAssertTrue(displayTodoListCalled)
+    }
+    
+    func testDidPressAddCallsGetAlertContentForAnOtherErrorWhenTodoIsEmpty() {
+        var displayedAlert: AlertContent?
+        sut.displayedAlert = { alertContent in displayedAlert = alertContent }
+        sut.didPressAdd(todo: "")
+        XCTAssertEqual(displayedAlert?.title, "Alert")
+        XCTAssertEqual(displayedAlert?.message, "An error has occurred, please try again later.")
+        XCTAssertEqual(displayedAlert?.cancelTitle, "Ok")
+    }
+
+    /* func testDidPressEditTodoDisplaysTextFieldAlert() {
+           let expectedTextFieldAlertContent = textFieldAlertContent(
+               title: "Alert",
+               message: "Edit your todo content.",
+               textField: "",
+               cancelTitle: "Ok"
+           )
+           var capturedTextFieldAlertContent: textFieldAlertContent?
+           sut.displayedTextFieldAlert = { textFieldAlertContent in
+               capturedTextFieldAlertContent = textFieldAlertContent
+           }
+           let index = 0
+           sut.didPressEditTodo(index: index, todoContent: "")
+           XCTAssertEqual(capturedTextFieldAlertContent, expectedTextFieldAlertContent)
+       } */
+       
+       func testChangeTodoValueWithEmptyContentCallsRemoveTodo() {
+           let index = 0
+           sut.changeTodoValue(content: "")
+           XCTAssertTrue(mockRepository.removeTodoCalled)
+           XCTAssertEqual(mockRepository.invokedRemoveTodoParameters, index)
+       }
+       
+       func testChangeTodoValueWithNonEmptyContentCallsEditTodo() {
+           let index = 0
+           let content = "New Todo Content"
+           sut.changeTodoValue(content: content)
+           XCTAssertTrue(mockRepository.editTodoCalled)
+           XCTAssertEqual(mockRepository.invokedEditTodoParameters?.index, index)
+           XCTAssertEqual(mockRepository.invokedEditTodoParameters?.content, content)
+       }
 }
 
+class MockListRepository: ListRepositoryType {
+    var addTodoCalled = false
+    var removeTodoCalled = false
+    var getTodosCalled = false
+    var editTodoCalled = false
+    var stubbedGetTodosResult: [String] = []
+    var invokedEditTodoParameters: (index: Int, content: String)?
+    var invokedRemoveTodoParameters: Int?
+    
+    func addTodo(content: String) {
+        addTodoCalled = true
+    }
 
+    func removeTodo(index: Int) {
+        removeTodoCalled = true
+        invokedRemoveTodoParameters = index
+    }
+
+    func getTodos() -> [String] {
+        getTodosCalled = true
+        return stubbedGetTodosResult
+    }
+
+    func editTodo(index: Int, content: String) {
+        editTodoCalled = true
+        invokedEditTodoParameters = (index: index, content: content)
+    }
+}
