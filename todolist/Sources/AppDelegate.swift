c04f52d5e231a83ae73e04e4a2a21efff5d15ea9
Valentin perello <perello.valentin@hotmail.fr>
2023-02-25 14:44:32 +0100

    Feat: Add google analytics

--- todolist/Sources/AppDelegate.swift
+++ todolist/Sources/AppDelegate.swift
@@ -7,6 +7,7 @@
 
 import UIKit
 import CoreData
+import FirebaseCore
 
 @main
 final class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -15,7 +16,10 @@ final class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        FirebaseApp.configure()
         return true
     }
 }
+
+// @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
 
