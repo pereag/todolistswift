2f0776a8de61a39bdde25ed20333349ab3aac5fd
Valentin perello <perello.valentin@hotmail.fr>
2023-03-04 10:09:50 +0100

    Feat: Ajout de google crashlitics

--- todolist/Sources/AppDelegate.swift
+++ todolist/Sources/AppDelegate.swift
@@ -7,6 +7,7 @@
 
 import UIKit
 import CoreData
+import FirebaseCore
 
 @main
 final class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -14,6 +15,7 @@ final class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        FirebaseApp.configure()
         // Override point for customization after application launch.
         return true
     }
