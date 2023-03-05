2b1ad746e14b3e2e18f495945db531a39d0ca770
Valentin perello <perello.valentin@hotmail.fr>
2023-03-05 09:29:02 +0100

    Fix: Error file not followinf

--- todolist/Sources/AppDelegate.swift
+++ todolist/Sources/AppDelegate.swift
@@ -15,7 +15,7 @@ final class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        FirebaseApp.configure()
+       FirebaseApp.configure()
         // Override point for customization after application launch.
         return true
     }
